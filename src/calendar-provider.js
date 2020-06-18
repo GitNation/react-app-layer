602058dda1d88a9ce4439c97d08c8245f7fd15a6
Oleg Proskurin <regx@usul.su>
2020-06-18 15:32:47 +0300

    Hotfix calendar links

--- src/calendar-provider.js
+++ src/calendar-provider.js
@@ -2,13 +2,25 @@ const ADD_EVENT_ID = 'aypbtNBcPzBIdDvukmvT46093';
 const TIMEZONE = 'Europe/Amsterdam';
 const FallbackStart = '2020-06-18 15:00';
 
+// TODO: obviously this patch should be removed for next events
+const magicHappensHere = ({ yyyy, mm, dd, time, track }) => {
+  const trackName = (track && track.name) || track;
+  if (trackName !== 'June 19') {
+    return null;
+  }
+  return `${yyyy}-${mm}-19 ${time}`;
+};
 
-export const createDateTime = (isoDate, time) => {
+export const createDateTime = (isoDate, time, track) => {
   try {
     const d = new Date(isoDate);
     const yyyy = d.getFullYear();
     const mm = d.getMonth() + 1;
     const dd = d.getDate();
+    const magicDate = magicHappensHere({ yyyy, mm, dd, time, track });
+    if (magicDate) {
+      return magicDate;
+    }
     return `${yyyy}-${mm}-${dd} ${time}`;
   } catch (err) {
     return FallbackStart;
@@ -26,7 +38,7 @@ export const createEventTitle = (speaker, talk) => {
 export const createCalendarLink = (speaker) => {
   const talk = speaker.activities && speaker.activities.talks[0];
   try {
-    const start = createDateTime(talk.isoDate, talk.time);
+    const start = createDateTime(talk.isoDate, talk.time, talk.track);
     const title = createEventTitle(speaker, talk);
 
     return `https://www.addevent.com/dir/?client=${ADD_EVENT_ID}&start=${start}&duration=${talk.duration}&title=${title}&timezone=${TIMEZONE}&alarm=15`;
