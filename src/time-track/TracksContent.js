b9c643e65509a1f29f3fac7e827f53bd36bd9e52
Oleg Proskurin <regx@usul.su>
2020-06-18 16:56:13 +0300

    Auto scroll v1.1.15

--- src/time-track/TracksContent.js
+++ src/time-track/TracksContent.js
@@ -1,11 +1,46 @@
 import React from 'react';
+
 import TracksHead from './TracksHead';
+import { generateTimeEvents } from '../time-provider';
+
+const autoScrollPosition = window.innerWidth * 0.2;
+
+/* TODO: move to separate custom hooks */
+const useCurrentTime = (calcPosition) => {
+  const [position, setPosition] = React.useState(null);
+
+  const updateTime = (time) => {
+    const pos = calcPosition(time);
+    setPosition(pos);
+  };
+
+  React.useEffect(() => {
+    generateTimeEvents({ cb: updateTime });
+  }, []);
+
+  return position;
+};
 
 function TracksContent({ children, timeTicks, trackWidth, calcPosition }) {
+  const [manualScroll, setManualScroll] = React.useState(false);
+  const trackEl = React.useRef(null);
+  const position = useCurrentTime(calcPosition);
+
+  React.useEffect(() => {
+    if (trackEl.current && position > 0 && position < trackWidth) {
+      trackEl.current.scroll(position - autoScrollPosition, 0);
+    }
+  }, [trackEl.current]);
+
   return (
-    <div className="time-track__content">
+    <div className="time-track__content" ref={trackEl}>
       <div className="time-track__content-inner">
-        <TracksHead timeTicks={timeTicks} style={{ width: trackWidth }} calcPosition={calcPosition}/>
+        <TracksHead
+          timeTicks={timeTicks}
+          style={{ width: trackWidth }}
+          trackWidth={trackWidth}
+          calcPosition={calcPosition}
+        />
         <div
           className="time-track__timeline"
           id="js-timeline"
