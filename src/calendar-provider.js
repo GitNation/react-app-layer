af78e26053141631d4314a50a9ac245f20b76c01
Robert Haritonov <1260461+operatino@users.noreply.github.com>
2021-03-23 18:32:11 +0000

    fix calendar event names, encode title and description, cleanup

--- src/calendar-provider.js
+++ src/calendar-provider.js
@@ -1,20 +1,9 @@
 const ADD_EVENT_ID = 'aypbtNBcPzBIdDvukmvT46093';
 import { DateTime } from 'luxon';
-const TIMEZONE = 'Europe/Amsterdam';
 
-const createDateTime = (isoDateWithoutTime, time) => {
-  const d = new Date(isoDateWithoutTime);
-  // get date always in original time zone
-  d.setTime(d.getTime() + d.getTimezoneOffset() * 60 * 1000);
-  const yyyy = d.getFullYear();
-  const mm = d.getMonth() + 1;
-  const dd = d.getDate();
-  return `${yyyy}-${mm}-${dd}${time ? ` ${time}` : ''}`;
-};
-
-const createEventTitle = (speaker, eventName) => {
+const createEventTitle = (speaker, talk, eventName) => {
   try {
-    return `${speaker.name || ''}${speaker.name ? ' - ' : ''}"${title}"${
+    return `${speaker.name || ''}${speaker.name ? ' - ' : ''}"${talk.title}"${
       eventName ? ` at ${eventName}` : ''
     }`;
   } catch (err) {
@@ -31,23 +20,25 @@ export const createCalendarLink = (
     conferenceEnd,
   },
 ) => {
+  const description = encodeURIComponent(calendarEventDescription);
+
   try {
     const talk = speaker.activities && speaker.activities.talks[0];
-    const title = createEventTitle(speaker, calendarEventName);
+    const title = encodeURIComponent(
+      createEventTitle(speaker, talk, calendarEventName),
+    );
 
     const localDate = DateTime.fromISO(talk.isoDate || talk.timeString);
     return `https://www.addevent.com/dir/?client=${ADD_EVENT_ID}&start=${localDate.toFormat(
       'yyyy/MM/dd HH:mm',
     )}&duration=${talk.duration}&title=${title}${
-      calendarEventDescription ? `&description=${calendarEventDescription}` : ''
+      description ? `&description=${description}` : ''
     }&timezone=${localDate.zoneName}&alarm=15`;
   } catch (err) {
     // Fallback whole day event
     if (calendarEventName && conferenceStart && conferenceEnd) {
       return `https://www.addevent.com/dir/?client=${ADD_EVENT_ID}&start=${conferenceStart}&end=${conferenceEnd}&title=${calendarEventName}${
-        calendarEventDescription
-          ? `&description=${calendarEventDescription}`
-          : ''
+        description ? `&description=${description}` : ''
       }&timezone=Europe/London&all_day_event=true`;
     }
 
