8e0d61e248e7003e95a231e67ec9d870148f4ae2
Robert Haritonov <1260461+operatino@users.noreply.github.com>
2022-10-14 13:58:17 +0100

    update api link to EMS, bump to 1.2.54

--- src/presence-tracker/App.js
+++ src/presence-tracker/App.js
@@ -2,26 +2,31 @@ import React, { useEffect } from 'react';
 
 import { getCookie } from '../services/getCookie';
 
-const INTERVAL = 5 * 60 * 1000
+const INTERVAL = 5 * 60 * 1000;
 
 export default function App({ bus }) {
-    useEffect(() => {
-        const { eventInfo: { emsEventId } } = bus.getContent();
-        const email = getCookie('watchMail');
+  useEffect(() => {
+    const {
+      eventInfo: { emsEventId },
+    } = bus.getContent();
+    const email = getCookie('watchMail');
 
-        if (emsEventId && email) {
-            const interval = setInterval(() => {
-                window.navigator.sendBeacon('https://portal.gitnation.org/api/live/track', JSON.stringify({
-                    eventId: emsEventId,
-                    email
-                }));
-            }, INTERVAL);
+    if (emsEventId && email) {
+      const interval = setInterval(() => {
+        window.navigator.sendBeacon(
+          'https://ems.gitnation.org/api/live/track',
+          JSON.stringify({
+            eventId: emsEventId,
+            email,
+          }),
+        );
+      }, INTERVAL);
 
-            return () => {
-                clearInterval(interval);
-            }
-        }
-    }, []);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, []);
 
-    return null;
+  return null;
 }
